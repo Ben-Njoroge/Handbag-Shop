c559d1a9d53581dc294c0e8fad41d122a21d681a
Ben Njoroge <njorogeben64@gmail.com>
2026-09-16 18:49:07 +0300

    Fixed Whitenoise dependencies and script formatting

--- BagCommerce/settings.py
+++ BagCommerce/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# This tells Vercel where to gather all the files during deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+# If you created the static folder in the main project directory instead of the app, you might need this:
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Enable Whitenoise to compress and cache the images in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Email
